6c5272df6f71be00c38583b4cac4cc36e6928817
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-10-23 20:54:56 +0100

    add archlinux pure install directory setup script

---
+++ scripts/puresetup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+root="$1";
+
+usage() {
+	echo "Usage: $0 rootprefixdir" >&2;
+	exit 1;
+}
+
+if test -z "$root"; then usage; fi;
+
+trymkdir() {
+	mkdir "$1" | exit $?;
+}
+
+trymkdir "$root/cfg";
+
+trymkdir "$root/pkgdata";
+#trymkdir "$root/pkgdata/syncdb";
+
+trymkdir "$root/var";
+trymkdir "$root/var/lib";
+trymkdir "$root/var/lib/pacman";
+ln -s "../../../pkgdata/syncdb" "$root/var/lib/pacman/sync" || exit $?;
